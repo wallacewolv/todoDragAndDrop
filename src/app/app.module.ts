bd0c9f406e06841ead12433117c97b3eb40b0eae
wallace <wallace_wolv@hotmail.com>
2023-07-15 20:22:42 -0300

    feat: Incluindo Drag and Drop List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 import { AppComponent } from './app.component';
 import { TarefaComponent } from './components/tarefa/tarefa.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    TarefaComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, TarefaComponent],
+  imports: [BrowserModule, DragDropModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/tarefa/tarefa.component.ts
+++ src/app/components/tarefa/tarefa.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tarefa',
   templateUrl: './tarefa.component.html',
-  styleUrls: ['./tarefa.component.css']
+  styleUrls: ['./tarefa.component.css'],
 })
-export class TarefaComponent {
+export class TarefaComponent implements OnInit {
+  todo: Array<string> = ['Fazer comida', 'Lavar a louça', 'Lavar o carro'];
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.todo, event.previousIndex, event.currentIndex);
+  }
 }
